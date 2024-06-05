d868dcb29a76e6747ee9aa4c29f8e693fb2329c3
An Qiuyu <qiuyu.an@hotmail.com>
2024-06-05 18:31:18 +0800

    Update k8s version and add support for Apple silicon (#54)
    - Update k8s version to 1.28
    - Add support for Apple silicon
    - Add support for provider vmware_fusion

--- labs/kubeadm-cluster/scripts/common.sh
+++ labs/kubeadm-cluster/scripts/common.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
+set -eu -o pipefail
 
-KUBERNETES_VERSION="1.21.1-00"
+KUBERNETES_VERSION="1.28.2-00"
 
 # disable swap 
 sudo swapoff -a
 # keeps the swap off during reboot
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release containerd bash-completion
 
@@ -17,6 +16,19 @@ sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-relea
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
+EOF
+
+sudo mkdir -p /etc/containerd
+cat <<EOF | sudo tee /etc/containerd/config.toml
+version = 2
+[plugins]
+  [plugins."io.containerd.grpc.v1.cri"]
+    [plugins."io.containerd.grpc.v1.cri".containerd]
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+        [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+          runtime_type = "io.containerd.runc.v2"
+          [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+            SystemdCgroup = true
 EOF
 
 sudo modprobe overlay
@@ -32,11 +44,14 @@ EOF
 # Apply sysctl params without reboot
 sudo sysctl --system
 
+sudo systemctl restart containerd
 sudo systemctl enable containerd --now
 
 echo "Container Runtime Configured Successfully"
 
-
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
 sudo apt-get install -y kubelet=$KUBERNETES_VERSION kubectl=$KUBERNETES_VERSION kubeadm=$KUBERNETES_VERSION
 sudo apt-mark hold kubelet kubeadm kubectl
 
